cfa081f5408167b246c4db59f00b4ce876a7342d
Remi ALBAN <remi@github.com>
2021-04-14 14:25:29 +0200

    Fix detection of ghost project

--- functions.py
+++ functions.py
@@ -2,14 +2,45 @@ import os
 import shutil
 import json
 
+def is_ghub_project(path=""):
+    if(os.path.exists(path + "ghub.json")):
+        return True
+    return False
+
+def create_json_file(name="my-project", path=""):
+    
+    if(os.path.exists(path + "ghub.json")):
+        print("You have already a ghub.json file")
+        exit()
+    fichier = open(path + "ghub.json", "x")
+    ghub = {
+        "name": name,
+        "commands": {
+            "test": "echo Hello world !"
+        }
+    }
+
+    fichier.write(json.dumps(ghub,indent = 4))
+    fichier.close()
+
 def help(command, value):
     print("erreur")
 
 def new(command, value):
-    os.system("git clone " + value[0])
+    if(len(value) == 2):
+        url = value[1]
+        name = value[0]
+    elif(len(value) == 1):
+        url = value[0]
+        name = "my-project"
+    else:
+        print("Erreur")
+        exit()
+    os.system("git clone " + url + " " + name)
+    create_json_file(name, name + "/")
 
 def remove(command, value):
-    if(os.path.exists(value[0] + "/.git")):
+    if(is_ghub_project(value[0] + "/")):
         shutil.rmtree(value[0])
     else:
         print("This directory does not exist or this directory is not a git project")
@@ -19,13 +50,13 @@ def push(command, value):
     if(len(value) == 1):
         message = value[0]
     print(message)
-    if(os.path.exists(".git")):
+    if(is_ghub_project()):
         os.system("git add -A")
         os.system("git commit -m \"" + message + "\"")
         os.system("git push")
 
 def update(command, value):
-    if(os.path.exists(".git")):
+    if(is_ghub_project()):
         os.system("git pull")
 
 def run(command, value):
@@ -45,19 +76,4 @@ def run(command, value):
     os.system(tab.get("commands").get(value[0]))
 
 def init(command, value):
-    if(os.path.exists("ghub.json")):
-        print("You have already a ghub.json file")
-        exit()
-    fichier = open("ghub.json", "x")
-    
-    ghub = {
-    "name": os.path.basename(os.getcwd()),
-    "commands": {
-        "test": "echo Hello world !"
-    }
-
-}
-    fichier.write(json.dumps(ghub,indent = 4))
-    fichier.close()
-
-print(os.getenv("HOME"))
+    create_json_file(os.path.basename(os.getcwd()))
--- ghub.py
+++ ghub.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-#sys.path.append('/home/remi/Documents/ghub')
+sys.path.append('/home/remi/Documents/ghub')
 from functions import *
 try:
     command = sys.argv[1]
@@ -15,12 +15,12 @@ except:
     pass
 
 switcher = {
-        "new": new,
-        "remove": remove,
-        "push": push,
-        "update": update,
-        "run": run,
-        "init": init,
-    }
+    "new": new,
+    "remove": remove,
+    "push": push,
+    "update": update,
+    "run": run,
+    "init": init,
+}
 
 switcher.get(command, help)(command, value)
